32ee23c9e12c0d3d60e50a953b725ca8f9631db2
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-03-30 15:51:19 +0900

    Time: 83 ms (20.20%), Space: 59.7 MB (18.25%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -3,26 +3,22 @@
  * @return {number}
  */
 const maxProfit = function(prices) {
-  let max;
-  let min;
-  let revenue;
+  let max = prices[prices.length - 1];
+  let min = prices[prices.length - 1];
+  let revenue = max - min;
   
   while (prices.length > 0) {
-    const last = prices.pop();
-    if (!max) {
-      max = last;
-    }
-    
+    const last = prices.pop();    
     if (last > prices[prices.length - 1] && last > max) {
       max = last
       min = last
     }
     
-    if (!min || min > last) {
+    if (min > last) {
       min = last;
     }
   
-    if (!revenue || revenue < max - min) {
+    if (revenue < max - min) {
       revenue = max - min;
     }
   }
