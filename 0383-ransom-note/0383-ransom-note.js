3fc0c76e292bc73b406638392358bede31952a73
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-03-31 16:41:21 +0900

    Time: 86 ms (15.56%), Space: 54.2 MB (22.36%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+const canConstruct = function(ransomNote, magazine) {
+  if (ransomNote.length > magazine.length) {
+    return false;
+  }
+  
+  const spellR = {};
+  const spellM = {};
+  
+  for (let i = 0; i < ransomNote.length; i++) {
+    if (!spellR[ransomNote[i]]) {
+      spellR[ransomNote[i]] = 0;
+    }
+    spellR[ransomNote[i]]++;
+  }
+  
+  for (let i = 0; i < magazine.length; i++) {
+    if (!spellM[magazine[i]]) {
+      spellM[magazine[i]] = 0;
+    }
+    spellM[magazine[i]]++;
+  }
+  
+  for (let spell in spellR) {
+    if (spellR[spell] > spellM[spell] || !spellM[spell]) {
+      return false;
+    }
+  }
+  
+  return true;
+};
