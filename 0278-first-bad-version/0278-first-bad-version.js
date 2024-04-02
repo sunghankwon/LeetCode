41156476b844ed9f60b7a642734f19a5213dd00b
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-04-02 14:51:16 +0900

    Time: 40 ms (96.69%), Space: 48.5 MB (66.83%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.js
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -20,10 +20,11 @@ const solution = function(isBadVersion) {
     while (start < end) {
       const mid = Math.floor((start + end) / 2);
       if (isBadVersion(mid)) {
-        end = mid;
         if (!isBadVersion(mid - 1)) {
-          return end;
+          return mid;
         }
+          
+        end = mid;
       } else {
         if (start === mid) {
           return end;
