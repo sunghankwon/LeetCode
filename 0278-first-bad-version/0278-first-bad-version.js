ba90779ce4275c0383257f3894b5dfcbcc2fdf99
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-04-02 14:45:42 +0900

    Time: 47 ms (75.12%), Space: 48.2 MB (88.88%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+const solution = function(isBadVersion) {
+  return function(n) {
+    let start = 1;
+    let end = n;
+    
+    while (start < end) {
+      const mid = Math.floor((start + end) / 2);
+      if (isBadVersion(mid)) {
+        end = mid;
+        if (!isBadVersion(mid - 1)) {
+          return end;
+      }
+      } else {
+        if (start === mid) {
+          return end;
+        }
+        start = mid
+      }
+    }
+
+    return end;
+  }
+};
