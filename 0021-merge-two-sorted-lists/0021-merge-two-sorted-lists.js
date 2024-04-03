5635afdf90fd50c1dc138f7494b9b2a407cc0e3f
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-04-03 22:44:15 +0900

    Time: 63 ms (52.72%), Space: 52.1 MB (17.88%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -1,15 +1,3 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} list1
- * @param {ListNode} list2
- * @return {ListNode}
- */
 const mergeTwoLists = function(l1, l2) {
   let newList = new ListNode(0);
   let newListHead = newList;
