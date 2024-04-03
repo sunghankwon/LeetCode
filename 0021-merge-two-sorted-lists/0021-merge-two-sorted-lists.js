801dbab11649c365d1ce0650c04efe9b2ccd64a1
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-04-03 22:43:07 +0900

    Time: 68 ms (27.27%), Space: 51.5 MB (55.35%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -15,7 +15,7 @@ const mergeTwoLists = function(l1, l2) {
   let newListHead = newList;
   
   while (l1 !== null && l2 !== null) {
-    if (l1 !== null && (l2 === null || l1.val < l2.val)) {
+    if (l2 === null || l1.val < l2.val) {
       newList.next = new ListNode(l1.val);
       l1 = l1.next;
     } else {
