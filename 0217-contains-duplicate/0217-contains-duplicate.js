2b67c5ab86e3770c36c6502a8ff5e1bbc51ced56
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-04-04 22:39:12 +0900

    Time: 82 ms (50.25%), Space: 61.7 MB (59.62%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -6,14 +6,10 @@ const containsDuplicate = function(nums) {
   const obj = {};
   
   for (let i = 0; i < nums.length; i++) {
-    if (!obj[nums[i]]) {
-      obj[nums[i]] = 0;
-    } 
-    obj[nums[i]]++;
-    
-    if (obj[nums[i]] === 2) {
+    if (obj[nums[i]]) {
       return true;
     }
+    obj[nums[i]] = true;
   }
   
   return false;
