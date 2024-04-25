1d1d63f662dad317242c92d76219d06e972c2336
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-04-25 15:16:12 +0900

    Time: 105 ms (11.61%), Space: 61.2 MB (15.61%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.js
+++ 0338-counting-bits/0338-counting-bits.js
@@ -1,14 +1,8 @@
-/**
- * @param {number} n
- * @return {number[]}
- */
 const countBits = function(n) {
   const result = [];
-  
   for (let i = 0; i < n + 1; i++) {
     const binaryScale = i.toString(2);
     result.push(binaryScale.split('1').length - 1);
   }
-  
   return result;
 };
