1954c7869c55eacb7872ce53d829df82cfb0c267
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-04-26 11:40:21 +0900

    Time: 84 ms (59.19%), Space: 56.3 MB (46.30%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -2,12 +2,6 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const sortedSquares = function(nums) {
-  const arr = [];
-  
-  for (let i = 0 ; i < nums.length; i++) {
-    nums[i] = nums[i] *nums[i];
-  }
-  
-  return nums.sort((a, b) => a - b);
+const sortedSquares = function(nums) {  
+  return nums.map(a => a * a).sort((a, b) => a - b);
 };
