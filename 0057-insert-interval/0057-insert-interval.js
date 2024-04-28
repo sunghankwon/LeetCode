e7d019934bc47f63f30c18e41d3b0496d1606072
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-04-28 16:45:26 +0900

    Time: 75 ms (59.06%), Space: 53.2 MB (93.07%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+const insert = function(intervals, newInterval) {
+  const arr = [...intervals].concat([newInterval]).sort((a,b) => a[0] - b[0]);
+  let currentS = arr[0][0];
+  let currentE = arr[0][1];
+  
+  for (let i = 1; i < arr.length; i++) {
+    if (currentE >= arr[i][0]) {
+      currentE = Math.max(arr[i][1], currentE);
+      arr[i - 1] = [currentS, currentE]
+      arr.splice(i, 1);
+      i--;
+    } else {
+      currentS = arr[i][0];
+      currentE = arr[i][1];
+    }
+  }
+  return arr;
+};
