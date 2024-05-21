84156f126420a06543380f4f3d1170149e434e94
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-05-21 17:50:23 +0900

    Time: 94 ms (8.06%), Space: 53.7 MB (27.38%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const addDigits = function(num) {
+  let str = num.toString();
+  
+  while(str.length > 1) {
+    let num = 0;
+    
+    for (let i = 0; i < str.length; i++) {
+      num += Number(str[i]);
+    }
+    str = num.toString();
+  }
+    
+  return Number(str);
+};
