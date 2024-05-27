f82e4e9aaf90cd19e8475b2cb6ea5167a236db9f
sunghan <138760807+sunghankwon@users.noreply.github.com>
2024-05-27 15:55:50 +0900

    Time: 123 ms (17.81%), Space: 62.4 MB (26.12%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const findDuplicates = function(nums) {
+  let obj = {};
+  let result = [];
+  
+  for (let i = 0; i < nums.length; i++) {
+    if (!obj[nums[i]]) {
+      obj[nums[i]] = 0;
+    }
+    obj[nums[i]]++;
+  }
+  
+  for (let num in obj) {
+    if(obj[num] === 2) {
+      result.push(Number(num))
+    }
+  }
+  
+  return result;
+};
